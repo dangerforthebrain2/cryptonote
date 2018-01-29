869e282b33ec799235af9a05f292c22cf8db13ad
Antony Sedgewick <dangerforthebrain2@users.noreply.github.com>
2018-01-29 03:03:25 +0000

    Add files via upload

--- CryptoNoteConfig.h
+++ CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "ec2-174-129-80-118.compute-1.amazonaws.com:8080",
+  "ec2-34-201-250-186.compute-1.amazonaws.com:17236",
   //"your_seed_ip2.com:8080",
 };
 
